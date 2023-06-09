de000c6ccb058685885b7052978d0e3318bbcc9f
SemiramisCavalcanti <webdesigncavalcanti@gmail.com>
2023-06-09 08:39:49 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sessi Sites</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <div id="desktop">
+        <video autoplay muted loop id="video-banner">
+            <source src="img/video-banner3.mp4" type="video/mp4">
+        </video>
+        <header>
+            <div class="container">
+                <img class="logo" src="img/logo.png" alt="">
+                <nav>
+                    <a href=""><i class="fa-brands fa-instagram"></i></a>
+
+                    <a href=""><i class="fa-solid fa-blog"></i></i></a>
+
+                    <a href=""><i class="fa-brands fa-linkedin"></i></a>
+                </nav>
+            </div>
+        </header>
+        <img src="img/brilho600.png" alt="" class="brilho-1">
+
+        <div class="text">
+            <h2>" JÁ IMAGINOU SUA EMPRESA NA AVENIDA </h2>
+            <h2>MAIS MOVIMENTADA DO MUNDO?? "</h2>
+        </div>
+        <div class="saindo">
+        </div>
+        <!-- <div id="triangulo"></div> -->
+        <div class="sobre">
+            <p class="text-2">Se você chegou neste site, já entendeu a importância de ter seu próprio terreno nesta Avenida. Já entendeu que o mercado vem mudando e cada vez mais digital.
+                <br>
+                Sou a Semíramis, da Sessi Sites, sou Desenvolvedora Web, e crio sites para prestadores de serviços, sou movida pela minha paixão de transmitir a essência das pessoas e suas marcas para as páginas web!
+            </p>
+            <div class="circulo-luz">
+                <!-- <img src="img/eu.jpg" alt=""> -->
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://kit.fontawesome.com/a1ee442963.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
